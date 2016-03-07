dfd993dd020900777f2b2cfff53a030d85044750
nerik <erik.escoffier@gmail.com>
2016-03-07 18:24:58 +0100

    3 choropleths

--- index.js
+++ index.js
@@ -12,8 +12,14 @@
     window.myapp.cssTemplates = {
       per_capita_income: cartodb._.template(cdb.$('#css_per_capita_income').html()),
       median_age: cartodb._.template(cdb.$('#css_median_age').html()),
+      ethnic_1st: cartodb._.template(cdb.$('#css_ethnic_1st').html()),
     }
 
+    window.myapp.htmlTemplates = {
+      drop: cartodb._.template( cdb.$('#html_drop').html() )
+    }
+
+    window.myapp.current_choro_widget = 0;
     window.myapp.station_id = 85;
     window.myapp.range = 1200;
     window.myapp.hists = {};
@@ -21,7 +27,7 @@
     window.myapp.vis.layers[1].options.layer_definition.layers[0].options.sql = window.myapp.sqlTemplates.blockgroups({data:{station_id: myapp.station_id, range: myapp.range, hists:myapp.hists}})
     window.myapp.vis.layers[1].options.layer_definition.layers[1].options.sql = window.myapp.sqlTemplates.dots({station_id: myapp.station_id, range: myapp.range})
     window.myapp.vis.layers[1].options.layer_definition.layers[2].options.sql = window.myapp.sqlTemplates.iso({station_id: myapp.station_id, range: myapp.range})
-    console.log(window.myapp.vis)
+
     window.myapp.diJSON = cdb._.extend(window.myapp.vis,
         {
         "title": "myapp Ads Demo",
@@ -130,11 +136,7 @@
 
           // inject dist selector
           var distSelector = cdb.$('.js-iso-selector');
-          distSelector.insertAfter(cdb.$('.CDB-Widget').eq(0))
-
-          // cdb.$('input[type="range"]').rangeslider({
-          //     polyfill: false,
-          //   })
+          distSelector.insertAfter(cdb.$('.CDB-Widget').eq(0));
 
           cdb.$('.js-Range').on('change', function(e) {
             myapp.range = parseInt(e.target.value);
@@ -153,50 +155,45 @@
             blockgroups.set('sql', blockgroups_sql_tpl);
           }
 
+          window.myapp.updateChoro = function (index) {
+            var col = ['per_capita_income','median_age','ethnic_1st'][index];
+            var cssTpl = window.myapp.cssTemplates[col]();
+            blockgroups.set('cartocss', cssTpl );
+          }
 
-          // listen to histogram changes
-          var onHistogramChange = function (model) {
-            // console.log(model)
-            if (model.get('histogram_sizes') === true) {
-              // console.log(model.get('column'));
+          // big motherfuckin hack
+          function onWidgetChange(model) {
+            console.log(model)
+            cdb.$('.CDB-Widget').eq(0).find('.CDB-Widget-options').remove();
+            cdb.$('.js-sizes').remove();
+            cdb.$('.js-colors').remove();
+            cdb.$('.js-colorsCustom').remove();
 
-              var col = model.get('column');
-              blockgroups.set('cartocss', window.myapp.cssTemplates[col]() );
-            }
-            // var min = model.filter.get('min');
-            // var max = model.filter.get('max');
-            //
-            // if (min && max) {
-            //   var column = model.get('column');
-            //   myapp.hists[column] = {
-            //     column: column,
-            //     min: min,
-            //     max: max,
-            //   };
-            //   var data = {
-            //     station_id: myapp.station_id,
-            //     range: myapp.range,
-            //     hists: myapp.hists
-            //   };
-            //
-            //   var blockgroups_sql_tpl = window.myapp.sqlTemplates.blockgroups({
-            //     data: data
-            //   });
-            //   console.log(data)
-            //   console.log(blockgroups_sql_tpl)
-            //   blockgroups.set('sql', blockgroups_sql_tpl);
-            // }
-          };
+            cdb.$('.CDB-Widget-options').each(function(i, el) {
+              var btn = cdb.$(window.myapp.htmlTemplates.drop({
+                isSelected: i === window.myapp.current_choro_widget,
+                index: i
+              }));
+              cdb.$(el).prepend(btn);
 
-          // cdb.$('.js-colors, ').on('click')
+              btn.on('click', function (e) {
+                var $t = (cdb.$(e.target).data('index')) ? cdb.$(e.target) : cdb.$(e.target).parent('.js-colorsCustom');
+                var index = parseInt($t.data('index'));
+                window.myapp.current_choro_widget = index;
+                cdb.$('.js-colorsCustom').removeClass('is-selected');
+                $t.addClass('is-selected')
+                window.myapp.updateChoro(index);
+              })
+            })
 
-          myapp.dash._dataviewsCollection.models[1].set('histogram_sizes', true);
-          myapp.dash._dataviewsCollection.models[1].on('change', onHistogramChange);
-          myapp.dash._dataviewsCollection.models[2].on('change', onHistogramChange);
+          }
+
+          myapp.dash._dataviewsCollection.models[1].on('change:data', onWidgetChange)
+          myapp.dash._dataviewsCollection.models[2].on('change:data', onWidgetChange)
+          myapp.dash._dataviewsCollection.models[3].on('change:data', onWidgetChange)
+
 
           myapp.dash._dataviewsCollection.models[3].on('change:data', function () {
-            console.log('BOOM');
-
             var recolor = function (title, color) {
                 if (cdb.$('.CDB-Widget-listItemInner:has(.CDB-Text[title="' + title + '"]) .CDB-Widget-progressState').get(0) != void 0) {
                     cdb.$('.CDB-Widget-listItemInner:has(.CDB-Text[title="' + title + '"]) .CDB-Widget-progressState').get(0).style.backgroundColor = color;
@@ -207,7 +204,9 @@
             recolor('hispanic or latino', '#ff8a00');
             recolor('white', '#a0a4d5');
             recolor('other', '#cccccc');
+
           });
+
 
 
           //fetch metro stations
@@ -227,9 +226,6 @@
                 layer.on('click', function (event) {
 
                   myapp.station_id = event.target.feature.properties.cartodb_id;
-
-                  // update cat widget colors
-
 
                   // update map
                   updateAll();
